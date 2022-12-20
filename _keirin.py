b28b7a62dd0d6a996c9b29b6492005a260dbd5be
hattorishota <hattorishota.eng@gmail.com>
2022-12-20 20:55:30 +0900

    競輪 データ収集

--- _keirin.py
+++ _keirin.py
@@ -3,6 +3,8 @@ from bs4 import BeautifulSoup
 import requests
 import time
 import tqdm
+import os
+import glob
 
 # 曜日ごとのレースのページを取得
 def createURL(month, day):
@@ -51,13 +53,43 @@ print(race_urls)
 # 現状：1レースごとにファイルが生成され、積み重なっていく。
 for n, race_url in tqdm.tqdm(enumerate(race_urls)):
 
+  if n == 0:
+    os.mkdir('race_data')
+
   # ページ内にあるtableタグの中身を取得
   r = pd.read_html(race_url, header = 0) 
 
   df = pd.DataFrame(r[0])
 
   # エクセルで出力
-  df.to_csv('result' + str(n) + '.csv')
+  df.to_csv('race_data/result' + str(n) + '.csv')
 
   # 3秒待機
   time.sleep(3)
+
+
+# csvファイルを結合する
+
+# # ディレクトリ変更
+# os.chdir('/Users/hattorishota1/github.com/scraping/race_data')
+
+# #1---フォルダ内のCSVファイルの一覧を取得
+# files = sorted(glob.glob('race_data/*.csv'))
+
+# #2---ファイル数を取得
+# file_number = len(files)
+
+# #3---CSVファイルの中身を読み出して、リスト形式にまとめる
+# csv_list = []
+
+# for file in files:
+#     csv_list.append(pd.read_csv(file))
+
+# #4---CSVファイルの結合
+# df = pd.concat(csv_list, axis = 0, sort = True)
+
+# #5---CSVファイル出力
+# df.to_csv("race_data.csv", index = False)
+
+# #6---完了合図
+# print(file_number,' 個のCSVファイルを結合完了！！')
